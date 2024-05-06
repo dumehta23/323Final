0f56673b42eefb2810ec60ba1b9616c320a79a50
dumehta23 <dumehta@csu.fullerton.edu>
2024-05-05 21:22:53 -0700

    this is the output of 'convert.py'

--- output.py
+++ output.py
@@ -1,7 +1,8 @@
-p1 = 3
-pp = 23
-q2s = 4
-r = 5
-p1 = 2 * p1 + r * (q2s + pp)
-pp = p1 * (q2s + 2 * r)
-print("value=" , pp)
+p1 = 3 
+pp = 23 
+q2s = 4 
+r = 5 
+p1 = 2 * p1 + r * (q2s + pp) 
+print(p1) 
+pp = p1 * (q2s + 2 * r) 
+print("value=" , pp) 
